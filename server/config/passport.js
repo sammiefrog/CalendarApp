7d89e2a3db9bf17346fdb065e12320fe9422dcfb
sammiefrog <sammiefrog@live.com>
2021-01-05 11:54:44 -0600

    brought in passport config files

---
+++ server/config/passport.js
@@ -0,0 +1,20 @@
+// passport configuration
+const User = require("../models/User");
+const { secret } = require("./keys");
+const JwtStrategy = require("passport-jwt").Strategy;
+const ExtractJwt = require("passport-jwt").ExtractJwt;
+
+const opts = {};
+
+opts.jwtFromRequest = ExtractJwt.fromAuthHeaderAsBearerToken();
+opts.secretOrKey = secret;
+
+module.exports = (passport) => {
+  passport.use(
+    new JwtStrategy(opts, (jwt_payload, done) => {
+      User.findOne({ id: jwt_payload.id })
+        .then((user) => (user ? done(null, user) : done(null, false)))
+        .catch((err) => done(err, false));
+    })
+  );
+};
