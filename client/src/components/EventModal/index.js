e8dfd6637bf92d8380c14c1fcfc783bb8b430fe9
Josh Everett <josheverett93@gmail.com>
2021-01-09 19:18:54 -0600

    created modal for adding, editing, and deleting events

--- client/src/components/EventModal/index.js
+++ client/src/components/EventModal/index.js
@@ -1,2 +1,79 @@
 // adding, viewing, and deleting events on calendar page
 // similar to pop up
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Modal from '@material-ui/core/Modal';
+
+function rand() {
+  return Math.round(Math.random() * 20) - 10;
+}
+
+function getModalStyle() {
+  const top = 50 + rand();
+  const left = 50 + rand();
+
+  return {
+    top: `${top}%`,
+    left: `${left}%`,
+    transform: `translate(-${top}%, -${left}%)`,
+  };
+}
+
+const useStyles = makeStyles((theme) => ({
+  paper: {
+    position: 'absolute',
+    width: 400,
+    backgroundColor: theme.palette.background.paper,
+    border: '2px solid #000',
+    boxShadow: theme.shadows[5],
+    padding: theme.spacing(2, 4, 3),
+  },
+}));
+
+export default function SimpleModal() {
+  const classes = useStyles();
+  const [modalStyle] = React.useState(getModalStyle);
+  const [open, setOpen] = React.useState(false);
+
+  const handleOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  const body = (
+    <div style={modalStyle} className={classes.paper}>
+      <h2 id="simple-modal-title">Event</h2>
+      <p id="simple-modal-description">
+        Duis mollis, est non commodo luctus, nisi erat porttitor ligula.
+      </p>
+      <SimpleModal />
+    </div>
+  );
+
+  return (
+    <div>
+      <button type="button" onClick={handleOpen}>
+        Add Event
+      </button>
+      <button type="button" onClick={handleOpen}>
+        Edit Event
+      </button>
+      <button type="button" onClick={handleOpen}>
+        Delete Event
+      </button>
+      <Modal
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="simple-modal-title"
+        aria-describedby="simple-modal-description"
+      >
+        {body}
+      </Modal>
+    </div>
+  );
+}
+
+// figure out if text field and/or date/time is best for input
