76e035b5d4062b5eac466f47474ea8b9bd9b8ac4
sammiefrog <sammiefrog@live.com>
2021-01-17 14:16:42 -0600

    added basic user routes

---
+++ server/routes/user-routes.js
@@ -0,0 +1,22 @@
+// importing required dependency and file for user
+const passport = require("passport");
+const {
+  RegistrationController,
+  LoginController,
+} = require("../controllers/userController");
+
+// routes for registration, login, and validation
+module.exports = (app) => {
+  app.post("/api/user/register", RegistrationController);
+
+  app.post("/api/user/login", LoginController);
+
+  app.post(
+    "/api/user/validate",
+    passport.authenticate("jwt", { session: false }),
+    (req, res) => {
+      console.log(req.user);
+      res.status(200).send("Authorized.");
+    }
+  );
+};
